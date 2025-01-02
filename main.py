dbb114d0783882770e6791f25fff5071edff274f
Thwisha-Vigneshwaran <thwisha1675@gmail.com>
2025-01-02 12:25:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 
-st.title('Project Title')
+st.title('Vinsup Project')
 with st.sidebar:
     selected = option_menu(
         menu_title='Project Title',
